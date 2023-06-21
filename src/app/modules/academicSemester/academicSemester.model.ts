f206d5071481288dec0421867a72eb99c5775b97
codewithshamim <shamimislamonline@gmail.com>
2023-06-21 20:53:06 +0600

    create academic semester interface&model

---
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -0,0 +1,43 @@
+import { Schema, model } from 'mongoose';
+import { Months } from './academicSemester.constant';
+import {
+  AcademicSemesterModel,
+  IAcademicSemester,
+} from './academicSemester.interface';
+
+const academicSemesterSchema = new Schema<IAcademicSemester>(
+  {
+    title: {
+      type: String,
+      enum: ['Autumn', 'Summer', 'Fall'],
+      required: true,
+    },
+    year: {
+      type: String,
+      required: true,
+    },
+    code: {
+      type: String,
+      enum: ['01', '02', '03'],
+      required: true,
+    },
+    startMonth: {
+      type: String,
+      required: true,
+      enum: Months,
+    },
+    endMonth: {
+      type: String,
+      required: true,
+      enum: Months,
+    },
+  },
+  { timestamps: true }
+);
+
+const AcademicSemester = model<IAcademicSemester, AcademicSemesterModel>(
+  'AcademicSemester',
+  academicSemesterSchema
+);
+
+export default AcademicSemester;
