d14256f031dcf26e20bc96ddd3caf5617d0b5ffa
codewithshamim <shamimislamonline@gmail.com>
2023-06-21 20:26:05 +0600

    updated

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -4,7 +4,11 @@ const createUserZodSchema = z.object({
   body: z.object({
     id: z.string().optional(),
     role: z.enum(['student', 'admin', 'faculty']).optional(),
-    password: z.string().optional(),
+    password: z
+      .string({
+        required_error: 'Password is required.',
+      })
+      .optional(),
   }),
 });
 
