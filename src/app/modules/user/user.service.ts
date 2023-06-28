50ebba95ccdd13354b89ab2ecf05ec19a16a4a7b
codewithshamim <shamimislamonline@gmail.com>
2023-06-28 11:13:24 +0600

    generate student, admin, faculty id

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,16 +1,22 @@
 import config from '../../../config';
 import generateUserId from '../../../utils/generateUserId';
+import { IAcademicSemester } from '../academicSemester/academicSemester.interface';
+import AcademicSemester from '../academicSemester/academicSemester.model';
 import { IUser } from './user.interface';
 import User from './user.model';
 
 const createUser = async (user: IUser): Promise<IUser> => {
-  // generate user id
-  const id = await generateUserId();
-  user.id = id;
-
-  // default role
   if (!user.role) {
     user.role = 'student';
+  }
+
+  // generate user id
+  const academicSemester = (await AcademicSemester.findOne({
+    code: '01',
+  })) as IAcademicSemester;
+  const id = await generateUserId(user.role, academicSemester);
+  if (id) {
+    user.id = id;
   }
 
   // default password
--- src/utils/generateUserId.ts
+++ src/utils/generateUserId.ts
@@ -1,20 +1,45 @@
+import { IAcademicSemester } from '../app/modules/academicSemester/academicSemester.interface';
 import User from '../app/modules/user/user.model';
 
-const findLastUserId = async () => {
-  const lastUser = await User.findOne({}, { id: 1, _id: 0 })
+type UserRole = 'student' | 'faculty' | 'admin';
+
+const findLastUserId = async (role: UserRole): Promise<string | undefined> => {
+  const lastUser = await User.findOne({ role }, { id: 1, _id: 0 })
     .sort({
       createdAt: -1,
     })
     .lean();
 
-  return lastUser?.id;
+  if (role === 'student') {
+    return lastUser?.id ? lastUser.id.substring(4) : undefined;
+  } else {
+    return lastUser?.id ? lastUser.id.substring(2) : undefined;
+  }
 };
 
-const generateUserId = async () => {
-  const currentId = (await findLastUserId()) || String(0).padStart(5, '0');
+const generateUserId = async (
+  role: UserRole,
+  academicSemester: IAcademicSemester
+): Promise<string | undefined> => {
+  const currentId = (await findLastUserId(role)) || String(0).padStart(5, '0');
 
   //increment by 1
-  const incrementedId = (parseInt(currentId) + 1).toString().padStart(5, '0');
+  let incrementedId;
+
+  if (role === 'student') {
+    incrementedId = `${academicSemester?.year?.substring(2)}${
+      academicSemester?.code
+    }${(parseInt(currentId) + 1).toString().padStart(5, '0')}`;
+  } else if (role === 'faculty') {
+    incrementedId = `F-${(parseInt(currentId) + 1)
+      .toString()
+      .padStart(5, '0')}`;
+  } else if (role === 'admin') {
+    incrementedId = `A-${(parseInt(currentId) + 1)
+      .toString()
+      .padStart(5, '0')}`;
+  }
+
   return incrementedId;
 };
 
